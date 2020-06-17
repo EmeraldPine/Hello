b70afb34e02869df2c1b2794a23286622100ade8
Ryan Chang <rmchang7@gmail.com>
2020-06-16 23:35:17 -0500

    image added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+
+    {/* <Image/> */}
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -15,7 +17,7 @@ const IndexPage = () => (
     {/* Should be linked styled as a button, not the other way around */}
     <Link to="/page-2/"><button className = "py-4 px-4 bg-blue-600 rounded text-white">Button One</button></Link> <br />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
