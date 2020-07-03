7c2d8abc140535ecdb0f04f73c42d90c382f1b9f
Ryan Chang <rmchang7@gmail.com>
2020-07-03 00:08:29 -0500

    Added footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
+import Footer from "../components/footer.jsx"
 
 const IndexPage = () => (
   <Layout>
@@ -21,6 +22,7 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Footer/>
   </Layout>
 )
 
